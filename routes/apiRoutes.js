62dacb62767bb2fc66dc8eb7a8a8a21faacc32e8
Leon Carnagie <leonfross@gmail.com>
2020-06-27 15:20:18 -0700

    Modified apiRoutes, htmlRoutes and server.js files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,36 @@
-var fs = require("fs"); 
-const { dirname } = require("path");
+var fs = require("fs");
+const path = require("path");
 
-module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-  
-    app.get("/api/notes", function(req, res) {
-        fs.readFile("./db/db.json",(err, data) =>{
-            if(err){
-                throw err
-            }
-            res.json(JSON.parse(data));
-        })
-      
-    });
-  
-  
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-  
-    app.post("/api/notes", function(req, res) {
-      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-      // It will do this by sending out the value "true" have a table
-      // req.body is available since we're using the body parsing middleware
-      if (tableData.length < 5) {
-        tableData.push(req.body);
-        res.json(true);
-      }
-      else {
-        waitListData.push(req.body);
-        res.json(false);
-      }
-    });
-  
-   
-  };
-  
+const router = require("express").Router();
+
+router.get("/notes", function (req, res) {
+  fs.readFile(path.resolve(__dirname, "../db/db.json"), (err, data) => {
+    if (err) {
+      throw err
+    }
+    res.json(JSON.parse(data));
+  })
+
+});
+
+
+
+router.post("/api/notes", function (req, res) {
+
+  if (tableData.length < 5) {
+    tableData.push(req.body);
+    res.json(true);
+  }
+  else {
+    waitListData.push(req.body);
+    res.json(false);
+  }
+});
+module.exports = router;
+
+// Hints:
+// Have an array of note
+// new note = req.body
+// push new note into array of note 
+// do fs write file which takes in path(db.jason) and data gitarray of note
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,15 @@
-var express = require("express");
+// var express = require("express");
+const path = require("path");
+
+const router = require("express").Router();
+
+router.get("/notes", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../public/notes.html"))
+
+});
+router.get("*", function (req, res) {
+    // console.log("reached route")
+    res.sendFile(path.resolve(__dirname, "../public/index.html"))
+
+});
+module.exports = router;
